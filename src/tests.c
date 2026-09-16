08a8be369c4cfd72fadaac02425c5bd5e3f718c7
Garrett Smart <gsmart@mtech.edu>
2026-09-16 16:49:02 -0600

    did test_find_last_single in tests.c

--- src/tests.c
+++ src/tests.c
@@ -104,7 +104,10 @@ void test_find_last_basic(void)
 
 void test_find_last_single(void)
 {
-    // TODO
+    char str[] = "e";
+    char *resultPtr = find_last_char(str);
+
+    TEST_ASSERT_EQUAL(str[0],*resultPtr);
 }
 
 
